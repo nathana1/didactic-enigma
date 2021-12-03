c945223edb6159bcb33c94611508f1dbd8fbc218
Nathan Anderson <nathana@microsoft.com>
2021-12-03 13:29:32 -0800

    update to add ratify

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,9 @@
 #!/bin/sh -l
-
-echo "Hello $1"
+# exit on first error and prevent filename expansion
+set -euf
+echo Hello $1
 time=$(date)
 echo "::set-output name=time::$time"
+
+jq --arg cert "$2" '.verifiers.plugins as $plugins | .verifiers.plugins[$plugins | map(.name == "notaryv2") | index(true)].verificationCerts[0] |= $cert' config.json > /.ratify/config.json
+/app/ratify verify -s "$3"
