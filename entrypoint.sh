b83f87daeea97ace18f587f8a7726c40e58ed988
Nathan Anderson <nathana@microsoft.com>
2021-12-03 13:40:41 -0800

    update config path

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,5 +5,5 @@ echo Hello $1
 time=$(date)
 echo "::set-output name=time::$time"
 
-jq --arg cert "$2" '.verifiers.plugins as $plugins | .verifiers.plugins[$plugins | map(.name == "notaryv2") | index(true)].verificationCerts[0] |= $cert' config.json > /.ratify/config.json
+jq --arg cert "$2" '.verifiers.plugins as $plugins | .verifiers.plugins[$plugins | map(.name == "notaryv2") | index(true)].verificationCerts[0] |= $cert' /config.json > /.ratify/config.json
 /app/ratify verify -s "$3"
