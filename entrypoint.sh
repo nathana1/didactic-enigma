67cab64edc89ccfc9ebc00529416c7ff892f5d71
Nathan Anderson <nathana@microsoft.com>
2021-12-10 15:01:51 -0800

    general cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh -l
 # exit on first error and prevent filename expansion
 set -euf
-echo Hello $1
-time=$(date)
-echo "::set-output name=time::$time"
 
-jq --arg cert "$2" '.verifiers.plugins as $plugins | .verifiers.plugins[$plugins | map(.name == "notaryv2") | index(true)].verificationCerts[0] |= $cert' /config.json > /.ratify/config.json
-/app/ratify verify -s "$3"
+jq --arg cert "$1" '.verifiers.plugins as $plugins | .verifiers.plugins[$plugins | map(.name == "notaryv2") | index(true)].verificationCerts[0] |= $cert' /config.json > /.ratify/config.json
+output=/app/ratify verify -s "$1"
+echo "::set-output name=verification::$output"
+success=$(cat output-false.json | jq '.isSuccess')
+if [ $success = 'true']
+then
+    exit 0
+fi
+
+exit 1
