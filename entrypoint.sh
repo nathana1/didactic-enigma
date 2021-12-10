72e76bdc482325c12d328892136bf20d7962feba
Nathan Anderson <nathana@microsoft.com>
2021-12-10 15:12:12 -0800

    fix typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,7 @@
 set -euf
 
 jq --arg cert "$1" '.verifiers.plugins as $plugins | .verifiers.plugins[$plugins | map(.name == "notaryv2") | index(true)].verificationCerts[0] |= $cert' /config.json > /.ratify/config.json
-output=$(/app/ratify verify -s "$1")
+output=$(/app/ratify verify -s "$2")
 echo "::set-output name=verification::$output"
 success=$(cat output-false.json | jq '.isSuccess')
 if [ $success = 'true']
